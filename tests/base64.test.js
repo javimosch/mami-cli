36065a519e1e354cd0e67805a5497cf737e4f81e
Javi <arancibiajav@gmail.com>
2019-04-01 22:57:32 -0300

    base64 test

--- tests/base64.test.js
+++ tests/base64.test.js
@@ -2,12 +2,5 @@ const {atob, btoa} = require('../utils')
 
 test('encode and decode a string gives the same value', () => {
 	let string = 'hello world'
-
-	if(false){
-		console.log('here')
-	}else{
-		console.log('there')
-	}
-
   expect(atob(btoa(string))).toBe(string);
 });
